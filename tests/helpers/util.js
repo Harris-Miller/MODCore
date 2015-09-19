9605bd5364a4fb3d50dc94627454c3dd3365d8a5
Harris Miller <harrismiller08@gmail.com>
2015-09-18 18:23:24 -0600

    unit tests working in both brower and node

--- tests/helpers/util.js
+++ tests/helpers/util.js
@@ -1,11 +1,16 @@
 
-var assert = require("assert");
+// check if running in node
+if (typeof window === 'undefined') {
+  require('chai').should();
+}
 
 describe('Array', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+      var arr = [1, 2, 3];
+
+      arr.indexOf(5).should.equal(-1);
+      arr.indexOf(0).should.equal(-1);
     });
   });
 });
