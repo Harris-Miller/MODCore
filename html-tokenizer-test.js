b8359153b5d62ca92bcde209c2a8bbacc7c3dcf1
Harris Miller <harrismiller08@gmail.com>
2015-09-22 02:01:53 -0600

    more work on template parse testing

--- html-tokenizer-test.js
+++ html-tokenizer-test.js
@@ -18,13 +18,13 @@ let inputs = tokens.filter(tokens => tokens.type === 'StartTag' && (tokens.tagNa
 // console.log(inputs);
 // console.log('');
 
-inputs.forEach(input => console.log(input.attributes));
-console.log('');
+// inputs.forEach(input => console.log(input.attributes));
+// console.log('');
 
 
 const stack = [];
 const script = [];
-const morphs = [];
+const morphs = []; // this is just so we know if a key alread exists in the script' morph variable
 
 /**
  * scrubs the tokens
@@ -67,7 +67,8 @@ function scrubTokens(tokens) {
 function createScript(tokens) {
   var nodes = 0;
 
-  script.push('var frag = document.createDocuemntFragment();');
+  script.push('var frag = document.createDocumentFragment();');
+  script.push('var morphs = {}');
 
   var current = 'frag';
 
@@ -77,6 +78,35 @@ function createScript(tokens) {
 
     if (type === 'Chars') {
       variable = 'e' + nodes++;
+      let chars = token.chars;
+      
+      // TODO: figure out how to separate the chars by `{{value}}`
+      // eg: "Hello there, {{first}} {{last}}. How are you!"
+      // -> [
+      //      ['string', 'Hello there, '],
+      //      ['mustache', 'first'],
+      //      ['string', ' '],
+      //      ['mustache', 'last'],
+      //      ['string', ' How are you!']
+      //    ]
+      //
+      // this way we can look through the tuples, and get and output of this:
+      //
+      // var e1 = document.createTextNode('Hello there, ');
+      // var frag.appendChild(e1);
+      // var e2 = document.createTextNode('');
+      // morphs['first'].push([e2, 'text']);
+      // var frag.appendChild(e2);
+      // var e3 = document.createTextNode(' ');
+      // var frag.appendChild(e3);
+      // var e4 = document.createTextNode('');
+      // morphs['last'].push([e4, 'text']);
+      // var frag.appendChild(e4);
+      // var e5 = document.createTextNode('. How are you!');
+      // var frag.appendChild(e5);
+      //
+      // by separating out the static and variable portions of text like this,
+      // we'll never have to re-render the entire sentence, only the mustache parts
 
       // TODO: html encode token.chars !!!
       script.push(`var ${variable} = document.createTextNode('${token.chars}');`);
@@ -95,12 +125,37 @@ function createScript(tokens) {
       if (attrs && Array.isArray(attrs)) {
         attrs.forEach(attr => {
           let name = attr[0];
-          let value = attr[1].replace('\\', '\\\\'); // escape
+          let values = attr[1] // escape ALL the things
+            .replace('\\', '\\\\')
+            .replace('\'', '\\\'')
+            .replace('\"', '\\\"');
 
           // TODO: figure out which keys are properties and which are attributes
-          // TODO: handle {{foo}}
+
+          // safe to assume that values are space delimited
+          values = values.split(/\s+/); // TOOD: move this to a variable
+
+          let mustaches = [];
+          values = values.filter(val => {
+            let mustache = val.match(/^\{\{(.*?)\}\}$/); // TODO: move this to a variable
+            if (mustache) {
+              mustaches.push(mustache[1]);
+            }
+            return !mustache;
+          });
+
+          // now we push
+          mustaches.forEach(mustache => {
+            if (morphs.indexOf(mustache) === -1) {
+              morphs.push(mustache);
+              script.push(`morphs['${mustache}'] = [];`);
+            }
+            script.push(`morphs['${mustache}'].push([${variable}, 'attr', '${name}']);`); // TODO: 'attr' may be 'prop' depending on 'name'
+          })
+          
           // TODO: only wrap second argument of setAttribute in a string if it needs it
-          script.push(`${variable}.setAttribute('${attr[0]}', '${attr[1]}');`);
+          // TODO: this may need to be a direct property set, depending on 'name'
+          script.push(`${variable}.setAttribute('${name}', '${values.join(' ')}');`);
         });
       }
 
@@ -116,6 +171,7 @@ function createScript(tokens) {
       return;
     }
     else if (type === 'EndTag') {
+      // when we reach and end tag, pop the stack and set to current
       current = stack.pop();
     }
 
@@ -126,6 +182,7 @@ scrubTokens(tokens);
 createScript(tokens);
 
 console.log(script);
+// console.log(morphs);
 
 fs.writeFileSync('script-output.js', script.join('\n'));
 
--- script-output.js
+++ script-output.js
@@ -1,50 +1,64 @@
-var frag = document.createDocuemntFragment();
-var e0 = document.createElement('form');
-e0.setAttribute('action', '');
+var frag = document.createDocumentFragment();
+var morphs = {}
+var e0 = document.createElement('h2');
 frag.appendChild(e0);
-var e1 = document.createTextNode('
-  ');
+var e1 = document.createTextNode('{{first}} {{last}}');
 e0.appendChild(e1);
-var e2 = document.createElement('fieldset');
-e2.setAttribute('class', 'info {{showing}}');
-e0.appendChild(e2);
-var e3 = document.createTextNode('
-    ');
-e2.appendChild(e3);
-var e4 = document.createElement('input');
-e4.setAttribute('type', 'text');
-e4.setAttribute('value', '{{first}}');
-e4.setAttribute('name', 'first');
-e2.appendChild(e4);
-var e5 = document.createTextNode('
-    ');
-e2.appendChild(e5);
-var e6 = document.createElement('br');
-e2.appendChild(e6);
-var e7 = document.createTextNode('
-    ');
-e2.appendChild(e7);
-var e8 = document.createElement('input');
-e8.setAttribute('type', 'text');
-e8.setAttribute('value', '{{last}}');
-e8.setAttribute('name', 'last');
-e2.appendChild(e8);
-var e9 = document.createTextNode('
-    ');
-e2.appendChild(e9);
-var e10 = document.createElement('br');
-e2.appendChild(e10);
-var e11 = document.createTextNode('
-    ');
-e2.appendChild(e11);
-var e12 = document.createElement('input');
-e12.setAttribute('type', 'text');
-e12.setAttribute('value', '31');
-e12.setAttribute('name', 'age');
-e2.appendChild(e12);
-var e13 = document.createTextNode('
-  ');
-e2.appendChild(e13);
-var e14 = document.createTextNode('
+var e2 = document.createTextNode('
 ');
-e0.appendChild(e14);
+frag.appendChild(e2);
+var e3 = document.createElement('form');
+e3.setAttribute('action', '');
+frag.appendChild(e3);
+var e4 = document.createTextNode('
+  ');
+e3.appendChild(e4);
+var e5 = document.createElement('fieldset');
+morphs['showing'] = [];
+morphs['showing'].push([e5, 'attr', 'class']);
+e5.setAttribute('class', 'info');
+e3.appendChild(e5);
+var e6 = document.createTextNode('
+    ');
+e5.appendChild(e6);
+var e7 = document.createElement('input');
+e7.setAttribute('type', 'text');
+morphs['first'] = [];
+morphs['first'].push([e7, 'attr', 'value']);
+e7.setAttribute('value', '');
+e7.setAttribute('name', 'first');
+e5.appendChild(e7);
+var e8 = document.createTextNode('
+    ');
+e5.appendChild(e8);
+var e9 = document.createElement('br');
+e5.appendChild(e9);
+var e10 = document.createTextNode('
+    ');
+e5.appendChild(e10);
+var e11 = document.createElement('input');
+e11.setAttribute('type', 'text');
+morphs['last'] = [];
+morphs['last'].push([e11, 'attr', 'value']);
+e11.setAttribute('value', '');
+e11.setAttribute('name', 'last');
+e5.appendChild(e11);
+var e12 = document.createTextNode('
+    ');
+e5.appendChild(e12);
+var e13 = document.createElement('br');
+e5.appendChild(e13);
+var e14 = document.createTextNode('
+    ');
+e5.appendChild(e14);
+var e15 = document.createElement('input');
+e15.setAttribute('type', 'text');
+e15.setAttribute('value', '31');
+e15.setAttribute('name', 'age');
+e5.appendChild(e15);
+var e16 = document.createTextNode('
+  ');
+e5.appendChild(e16);
+var e17 = document.createTextNode('
+');
+e3.appendChild(e17);
