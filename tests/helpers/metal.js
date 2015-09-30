6301f77f208f3e7fca65eced631f280a1fe343fc
Harris Miller <harrismiller08@gmail.com>
2015-09-29 21:04:53 -0600

    added most unit tests

--- tests/helpers/metal.js
+++ tests/helpers/metal.js
@@ -24,4 +24,52 @@ describe('helpers/metal', function() {
 		});
 	});
 
+	describe('#makeArray(obj)', function() {
+		var makeArray = metal.makeArray;
+
+		it('should return an empty Array if obj isNone', function() {
+
+			var fromNothing = makeArray();
+			Array.isArray(fromNothing).should.be.true;
+			fromNothing.should.be.empty;
+
+			var fromNull = makeArray(null);
+			Array.isArray(fromNull).should.be.true;
+			fromNull.should.be.empty;
+
+			var fromUndefined = makeArray(undefined);
+			Array.isArray(fromUndefined).should.be.true;
+			fromUndefined.should.be.empty;
+		});
+
+		it('should return obj if obj is an array', function() {
+			var arr = [];
+
+			makeArray(arr).should.equal(arr);
+		});
+
+		it('should wrap any other obj in an array', function() {
+
+			var str = 'string';
+			var arrStr = makeArray(str);
+			Array.isArray(arrStr).should.be.true;
+			arrStr[0].should.equal(str);
+
+			var num = 1;
+			var arrNum = makeArray(num);
+			Array.isArray(arrNum).should.be.true;
+			arrNum[0].should.equal(num);
+
+			var obj = {};
+			var arrObj = makeArray(obj);
+			Array.isArray(arrObj).should.be.true;
+			arrObj[0].should.equal(obj);
+
+			var func = function() { };
+			var arrFunc = makeArray(func);
+			Array.isArray(arrFunc).should.be.true;
+			arrFunc[0].should.equal(func);
+		});
+	});
+
 });
